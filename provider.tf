6af54e22a92de8559ad1aeb15517b0163fde434c
srinu <srinu123@gmail.com>
2023-01-12 12:49:20 +0530

    added notes

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+
+    access_key = "AKIAXFCKTTDNVNALWGDQ"
+    secret_key = "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"
+    region =  "us-west-2"
+    
+  
+}
---
+++ resource.tf
@@ -0,0 +1,10 @@
+resource "aws_vpc" "myvpc" {
+  
+      cidr_block = "10.0.0.0/16"
+
+      tags = {
+
+        name = "my_vpc"
+        ENV = "PROD"
+      }
+}
