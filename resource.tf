6f9a737ba3ae9a885bbe5e77324fb0c595867c32
srinu <srinu123@gmail.com>
2023-01-13 08:09:14 +0530

    added notes

--- resource.tf
+++ resource.tf
@@ -8,3 +8,38 @@ resource "aws_vpc" "myvpc" {
         ENV = "PROD"
       }
 }
+
+resource "aws_subnet" "my_subnet1" {
+
+  vpc_id = aws_vpc.myvpc
+
+  cidr_block = "10.0.1.0/24"
+  
+  tags = {
+    "name" = "firstsubnet"
+  }
+}
+resource "aws_subnet" "my_subnet2" {
+  vpc_id = aws_vpc.myvpc
+  cidr_block = "10.0.2.0/24"
+  tags = {
+    "name" = "secondsubnet"
+  }
+  
+}
+resource "aws_subnet" "my_subnet3" {
+  vpc_id = aws_vpc.myvpc
+  cidr_block = "10.0.3.0/24"
+  tags = {
+    "name" = "thirdsubnet"
+  }
+  
+}
+resource "aws_subnet" "my_subnet4" {
+  vpc_id = aws_vpc.myvpc
+  cidr_block = "10.0.4.0/24"
+  tags = {
+    "name" = "fourthsubnet"
+  }
+  
+}
